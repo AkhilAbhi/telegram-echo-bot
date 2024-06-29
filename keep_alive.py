5a99847657473e1608eaf8efed9125202d979436
AkhilAbhi <66011589+AkhilAbhi@users.noreply.github.com>
2024-06-29 20:40:18 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,8 +1,9 @@
 from flask import Flask,render_template
 from threading import Thread
+import os
 
 app = Flask(__name__)
-
+port =os.environ.get('PORT')
 @app.route('/')
 def index():
     return "Alive"
